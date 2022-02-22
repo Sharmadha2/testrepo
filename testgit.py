72c957e4887a4fd00f15c3bd57d3bdf4b907af80
Sharmadha2 <86168271+Sharmadha2@users.noreply.github.com>
2022-02-22 15:51:21 +0100

    Update testgit.py

--- testgit.py
+++ testgit.py
@@ -3,12 +3,11 @@ if sample == "yes":
     print('ok')
     print('okay')
 else:
-<<<<<<< HEAD
+
     print('no')
     print('sorry')
-=======
+
     new =input("are you alright")
     if new =="yes":
         print('healthy')
 
->>>>>>> new
